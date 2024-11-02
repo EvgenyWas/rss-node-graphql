95817da9e70b695e4b85ad1cf21c5209d5c77e6c
EvgenyWas <evgeniywas@gmail.com>
2024-11-02 09:25:58 +0000

    feat: create gql schema

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,13 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLSchema } from 'graphql';
+
+import { Mutation } from './mutation.js';
+import { Query } from './query.js';
+import { memberTypes } from './types/member.js';
+import { postTypes } from './types/post.js';
+import { profileTypes } from './types/profile.js';
+import { userTypes } from './types/user.js';
+import { UUIDType } from './types/uuid.js';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +27,9 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+export const gqlSchema = new GraphQLSchema({
+  query: Query,
+  mutation: Mutation,
+  types: [...memberTypes, ...postTypes, ...profileTypes, ...userTypes, UUIDType],
+});
